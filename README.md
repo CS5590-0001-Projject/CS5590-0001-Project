64d59ab5e682f89ac1382038ca6b261c975b0707
MolanZ <mz9kk@mail.umkc.edu>
2020-12-09 23:29:08 -0600

    update

--- README.md
+++ README.md
@@ -1,7 +1,7 @@
 # CS5590-0001-Project
 
  One Circle created by Team 1.
- 
+
 ## One Circle
 ### Team details
 
@@ -11,45 +11,43 @@
  ●    Benjamin Nguyen: Computer Science Major; University of Missouri -- Kansas City, pdnrtv@umsystem.edu
 
  ### The story and its details
- TODO 
- 
+
+ There are people and communities in need of help: company managers, policemen, FBI agents, and CIA agents. The story can be divided into two parts: enterprises and the public security department.
+For enterprises, managers are pretty busy. They are facing the highest pressures every day, they need to make those employees work most efficiently and they need to make the most important decision for the company. They do not have much more time for taking a recording of those employees’ attendance. As a result, the managers have to lighten the burden in their hands. When managers want to know the employee's monthly attendance table, the problem will take place. The model we trained can record attendance of each employee and provide a daily report to their managers on the computer screen in their office rooms. The reason why managers use One Circle is managers do not have extra time spent recording employee's attendance.
+For the public security department such as police departments or FBI departments, it is hard to find criminals on surveillance video/photo by eyes. When policemen or agents try to find criminals as quickly as possible, One Circle can help them. The model we trained can track a specific face when this face appears on surveillance video/photo, One Circle will send a short report to let agents/policemen know where the person is by using Trilateration[2]. The reason why agents/policemen use One Circle is they cannot work all day all night to check each surveillance video/photo to find criminals.
+
+
  ### The data and its details
 
  In this project I use the Labeled Faces in the Wild Dataset[1] as our training and testing dataset. This is a face photo database designed for studying the problem of face recognition in the natural environment. The dataset contains more than 13,000 images collected from the Internet. Each face is marked with the person’s name. This dataset contains a total 5749 person’s photos, of which 1,680 people have two or more different photos, which are captured by the Viola-Jones face detector.  Some of samples can be seen below:
 ![Image text](https://github.com/CS5590-0001-Projject/CS5590-0001-Project/blob/main/screenshot/dataset.JPG)
 
- Given that the ideal application scenario of this model is the company's commuting records and the pursuit of suspects by the public security organs, the input should come from real life. For the company, the input photos of the model may be taken in the office building. For public security organs, the pictures are more from roads, shopping malls and other public places. 
+ Given that the ideal application scenario of this model is the company's commuting records and the pursuit of suspects by the public security organs, the input should come from real life. For the company, the input photos of the model may be taken in the office building. For public security organs, the pictures are more from roads, shopping malls and other public places.
 
- ### Working screens from project
- 
- ●    Training process
- ![Image text](https://github.com/CS5590-0001-Projject/CS5590-0001-Project/blob/main/screenshot/training_process.JPG)
- 
- This image shows the training process. Here I trained the Triplet network for 10 epches. And for the linear classification network I trained 100 epches. It takes about one hour.
- 
- ●    Training and validation loss rate
- ![Image text](https://github.com/CS5590-0001-Projject/CS5590-0001-Project/blob/main/screenshot/loss.JPG)
- 
- ●    Training and validation accuracy rate
- ![Image text](https://github.com/CS5590-0001-Projject/CS5590-0001-Project/blob/main/screenshot/acc.JPG)
- 
- ●    Test result analysis
-        ●    Confusion matrix
-        ●    Classification report
-        ●    Conlusion
-        
-●    Some testing samples
+ ### Modle
 
+ Details can be seen in our report. Code can be found in "code" folder.
 
 ### Work sharing/Module sharing between teammates
- TODO 
- 
-### Any issues, blockages with the project
- TODO 
- 
-### GitHub link for your project
- ●    https://github.com/jun0405/umkc_one_circle
- 
- ### References
- ●    Huang, G. B., Mattar, M., Berg, T., & Learned-Miller, E. (2008, October). Labeled faces in the wild: A database for studying face recognition in unconstrained environments.
 
+Jingtang Ma (Arthur): Work with the story and its details; Help Molan evaluating the model; Communication with the instructor; Come up with the agenda and communicate with the team.
+Molan Zhang: Work with the data and its details; Work with the code; design model and improve it; evaluating model; record video.
+Qiao Yang: organizing the report, code review, and testing
+Benjamin Nguyen: Finding issues, blockages during projects (in reality and program), running, testing, reviewing the code to collect issues. Contribution in story and details.
+
+### Any issues, blockages with the project
+
+In reality, we have to face privacy problems. Security and privacy are issues we need to concern. Need to find a way to make sure these data of face we record are used in the safe and right way and doesn’t leak the data because information is very important. In addition, the program can be used for some bad purposes such as spying or selling dataset, this violates the privacy permission. Our program cannot guarantee that the result is  great  but we make it as similar as possible with the realistic model because there is probably the possibility of misrecognition, which leads to wrong decisions. It is supposed to be dangerous if government or police agents make wrongful convictions.
+
+While working with the code, one of the problems that we face is there are too many packages that are used such as pytorch (including torchvision, cuda, optim), it took time to find out the way to install and import. Second, the problem with CUDA enabled and the torch, it requires a GPU that is strong enough to process so it may be hard to run with a weak GPU. Moreover, CUDA version should be compatible with torch, it means we need to check the CUDA version before installing torch. It will catch errors if CUDA enabled a different version with torch used. Third, training and testing data took a significant time till the final result during each epoch, but if we want the most accurate result, we need to use enough needed epochs to catch a good result. Another issue, it is not important, but using VGG-16 may not change the accuracy score significantly (as we can see in the graph), and it makes the model’s weight heavy, increasing interference time. It obviously catches good results eventually.
+
+For this model usage, we also need to consider the loss cost for misclassification. For example, the cost of missing a criminal is larger (false positive) than identifying a wrong person as a criminal by mistake (false negative). In the result, we might use the BoostCost classifier as the final classification model which can create our own cost function. Also, due to the specifical edition for Python and some packages, it is hard to transfer this model to the other different environments. We also plan to put this model in a docker when we finish the training process.
+
+### Video link for your project
+
+### Presentation link for your project
+
+### References
+
+Huang, G. B., Mattar, M., Berg, T., & Learned-Miller, E. (2008, October). Labeled faces in the wild: A database for studying face recognition in unconstrained environments.
+L. A. Martínez Hernández, S. Pérez Arteaga, G. Sánchez Pérez, A. L. Sandoval Orozco and L. J. García Villalba, "Outdoor Location of Mobile Devices Using Trilateration Algorithms for Emergency Services," in IEEE Access, vol. 7, pp. 52052-52059, 2019, doi: 10.1109/ACCESS.2019.2911058.
--- code/CS5590-0001-Project.py
+++ code/CS5590-0001-Project.py
@@ -11,7 +11,7 @@ from os import listdir
 import torch
 import torch.nn as nn
 import torchvision.models as models
-from torch.autograd import Variable 
+from torch.autograd import Variable
 import torch.cuda
 import torchvision.transforms as transforms
 import skimage.io
@@ -40,60 +40,60 @@ transform = transforms.Compose([transforms.ToTensor(),
 
 
 class VGG16(nn.Module):
-    
-    
+
+
     def __init__(self):
         super(VGG16, self).__init__()
-        
+
         # 3 * 224 * 224
         self.conv1_1 = nn.Conv2d(3, 64, 3) # 64 * 222 * 222
         self.conv1_2 = nn.Conv2d(64, 64, 3, padding=(1, 1)) # 64 * 222* 222
         self.maxpool1 = nn.MaxPool2d((2, 2), padding=(1, 1)) # pooling 64 * 112 * 112
-        
+
         self.conv2_1 = nn.Conv2d(64, 128, 3) # 128 * 110 * 110
         self.conv2_2 = nn.Conv2d(128, 128, 3, padding=(1, 1)) # 128 * 110 * 110
         self.maxpool2 = nn.MaxPool2d((2, 2), padding=(1, 1)) # pooling 128 * 56 * 56
-        
+
         self.conv3_1 = nn.Conv2d(128, 256, 3) # 256 * 54 * 54
         self.conv3_2 = nn.Conv2d(256, 256, 3, padding=(1, 1)) # 256 * 54 * 54
         self.conv3_3 = nn.Conv2d(256, 256, 3, padding=(1, 1)) # 256 * 54 * 54
         self.maxpool3 = nn.MaxPool2d((2, 2), padding=(1, 1)) # pooling 256 * 28 * 28
-        
+
         self.conv4_1 = nn.Conv2d(256, 512, 3) # 512 * 26 * 26
         self.conv4_2 = nn.Conv2d(512, 512, 3, padding=(1, 1)) # 512 * 26 * 26
         self.conv4_3 = nn.Conv2d(512, 512, 3, padding=(1, 1)) # 512 * 26 * 26
         self.maxpool4 = nn.MaxPool2d((2, 2), padding=(1, 1)) # pooling 512 * 14 * 14
-        
+
         self.conv5_1 = nn.Conv2d(512, 512, 3) # 512 * 12 * 12
         self.conv5_2 = nn.Conv2d(512, 512, 3, padding=(1, 1)) # 512 * 12 * 12
         self.conv5_3 = nn.Conv2d(512, 512, 3, padding=(1, 1)) # 512 * 12 * 12
         self.maxpool5 = nn.MaxPool2d((2, 2), padding=(1, 1)) # pooling 512 * 7 * 7
-        
+
         # view
-        
+
         self.fc1 = nn.Linear(512 * 7 * 7, 4096)
         self.fc2 = nn.Linear(4096, 4096)
         self.fc3 = nn.Linear(4096, 2048)
         # softmax 1 * 1 * 1000
-        
+
     def forward_once(self, x):
-        
+
         # x.size(0) is batch_size
         in_size = x.size(0)
-        
+
         out = self.conv1_1(x) # 222
         out = F.relu(out)
-        
+
         out = self.conv1_2(out) # 222
         out = F.relu(out)
         out = self.maxpool1(out) # 112
-        
+
         out = self.conv2_1(out) # 110
         out = F.relu(out)
         out = self.conv2_2(out) # 110
         out = F.relu(out)
         out = self.maxpool2(out) # 56
-        
+
         out = self.conv3_1(out) # 54
         out = F.relu(out)
         out = self.conv3_2(out) # 54
@@ -101,7 +101,7 @@ class VGG16(nn.Module):
         out = self.conv3_3(out) # 54
         out = F.relu(out)
         out = self.maxpool3(out) # 28
-        
+
         out = self.conv4_1(out) # 26
         out = F.relu(out)
         out = self.conv4_2(out) # 26
@@ -109,7 +109,7 @@ class VGG16(nn.Module):
         out = self.conv4_3(out) # 26
         out = F.relu(out)
         out = self.maxpool4(out) # 14
-        
+
         out = self.conv5_1(out) # 12
         out = F.relu(out)
         out = self.conv5_2(out) # 12
@@ -117,10 +117,10 @@ class VGG16(nn.Module):
         out = self.conv5_3(out) # 12
         out = F.relu(out)
         out = self.maxpool5(out) # 7
-        
+
         # reshape as 2D
         out = out.view(in_size, -1)
-        
+
         out = self.fc1(out)
         out = F.relu(out)
         out = self.fc2(out)
@@ -128,7 +128,7 @@ class VGG16(nn.Module):
         out = self.fc3(out)
         #out = F.log_softmax(out, dim=1)
         return out
-                
+
     def forward(self, input1, input2=None, input3=None):
         if input2 is None and input3 is None:
             output1 = self.forward_once(input1)
@@ -142,96 +142,6 @@ class VGG16(nn.Module):
 
 # In[4]:
 
-
-def Conv1(in_planes, places, stride=2):
-    return nn.Sequential(
-        nn.Conv2d(in_channels=in_planes,out_channels=places,kernel_size=7,stride=stride,padding=3, bias=False),
-        nn.BatchNorm2d(places),
-        nn.ReLU(inplace=True),
-        nn.MaxPool2d(kernel_size=3, stride=2, padding=1)
-    )
-
-class Bottleneck(nn.Module):
-    def __init__(self,in_places,places, stride=1,downsampling=False, expansion = 4):
-        super(Bottleneck,self).__init__()
-        self.expansion = expansion
-        self.downsampling = downsampling
-
-        self.bottleneck = nn.Sequential(
-            nn.Conv2d(in_channels=in_places,out_channels=places,kernel_size=1,stride=1, bias=False),
-            nn.BatchNorm2d(places),
-            nn.ReLU(inplace=True),
-            nn.Conv2d(in_channels=places, out_channels=places, kernel_size=3, stride=stride, padding=1, bias=False),
-            nn.BatchNorm2d(places),
-            nn.ReLU(inplace=True),
-            nn.Conv2d(in_channels=places, out_channels=places*self.expansion, kernel_size=1, stride=1, bias=False),
-            nn.BatchNorm2d(places*self.expansion),
-        )
-
-        if self.downsampling:
-            self.downsample = nn.Sequential(
-                nn.Conv2d(in_channels=in_places, out_channels=places*self.expansion, kernel_size=1, stride=stride, bias=False),
-                nn.BatchNorm2d(places*self.expansion)
-            )
-        self.relu = nn.ReLU(inplace=True)
-    def forward(self, x):
-        residual = x
-        out = self.bottleneck(x)
-
-        if self.downsampling:
-            residual = self.downsample(x)
-
-        out += residual
-        out = self.relu(out)
-        return out
-
-class SiameseNetwork(nn.Module):
-    def __init__(self,blocks, num_classes=1000, expansion = 4):
-        super(SiameseNetwork,self).__init__()
-        self.expansion = expansion
-
-        self.conv1 = Conv1(in_planes = 9, places= 64)
-
-        self.layer1 = self.make_layer(in_places = 64, places= 64, block=blocks[0], stride=1)
-        self.layer2 = self.make_layer(in_places = 256,places=128, block=blocks[1], stride=2)
-        self.layer3 = self.make_layer(in_places=512,places=256, block=blocks[2], stride=2)
-        self.layer4 = self.make_layer(in_places=1024,places=512, block=blocks[3], stride=2)
-
-        self.avgpool = nn.AvgPool2d(7, stride=1)
-        # self.fc = nn.Linear(2048,num_classes)
-
-    def make_layer(self, in_places, places, block, stride):
-        layers = []
-        layers.append(Bottleneck(in_places, places,stride, downsampling =True))
-        for i in range(1, block):
-            layers.append(Bottleneck(places*self.expansion, places))
-
-        return nn.Sequential(*layers)
-
-    def forward_once(self, x):
-        x = x.type(torch.cuda.FloatTensor)
-        x = self.conv1(x)
-        x = self.layer1(x)
-        x = self.layer2(x)
-        x = self.layer3(x)
-        x = self.layer4(x)
-        x = self.avgpool(x)
-        x = x.view(x.size(0), -1)
-        # x = self.fc(x)
-        return x
-    
-    def forward(self, input1, input2=None, input3=None):
-        if input2 is None and input3 is None:
-            output1 = self.forward_once(input1)
-            return output1
-        else:
-            output1 = self.forward_once(input1)
-            output2 = self.forward_once(input2)
-            output3 = self.forward_once(input3)
-            return output1, output2, output3
-
-
-# In[6]:
 
 
 class Net(torch.nn.Module): #This is a linear claffification model used to classify each person
@@ -270,7 +180,7 @@ class ContrastiveLoss(torch.nn.Module):
 
 
 class MyDataset(torch.utils.data.Dataset): #Create my dataset which inherits from torch.utils.data.Dataset
-    def __init__(self,txt, transform=None, target_transform=None): 
+    def __init__(self,txt, transform=None, target_transform=None):
         super(MyDataset,self).__init__()
         path=txt
         file_list=open(path,'r')
@@ -278,20 +188,20 @@ class MyDataset(torch.utils.data.Dataset): #Create my dataset which inherits fro
         for line in file_list:
             line = line.rstrip()
             words = line.split()
-            imgs.append((words[0],words[1], words[2], words[3])) 
+            imgs.append((words[0],words[1], words[2], words[3]))
         self.imgs = imgs
         self.transform = transform
         self.target_transform = target_transform
- 
+
     def __getitem__(self, index):
-        an_name, anchor, po_name, postive = self.imgs[index] 
+        an_name, anchor, po_name, postive = self.imgs[index]
         an_path = '/home/molan/Desktop/lfw/'+an_name+'/'+an_name+'_'+str(anchor).zfill(4)+'.jpg'
         po_path = '/home/molan/Desktop/lfw/'+po_name+'/'+po_name+'_'+str(postive).zfill(4)+'.jpg'
         if an_name == po_name :
             label = 1
         else:
             label = 0
-            
+
         file_lis=listdir('/home/molan/Desktop/lfw/')
         file_lis.remove(po_name)
         ne_name = random.choice(file_lis)
@@ -304,16 +214,16 @@ class MyDataset(torch.utils.data.Dataset): #Create my dataset which inherits fro
         img_po = cv2.resize(img_po,(224,224))
         img_ne = cv2.imread(ne_path)
         img_ne = cv2.resize(img_ne,(224,224))
-        
+
         if self.transform is not None:
             img = self.transform(img_an) #transform images as we defined before
             exp_po = self.transform(img_po)
             exp_ne = self.transform(img_ne)
         return img,exp_po,exp_ne,label,an_name
- 
-    def __len__(self): 
+
+    def __len__(self):
         return len(self.imgs)
-    
+
 
 
 # In[41]:
@@ -346,7 +256,7 @@ def extract_feature(net):
                 label.append(label_num)
             label_name[name]=label_num
             label_num += 1
-    return feature,label,label_name    
+    return feature,label,label_name
 
 
 # In[10]:
@@ -354,11 +264,12 @@ def extract_feature(net):
 
 #load training and testing data
 train_data=MyDataset(txt='/home/molan/Desktop/project/train_new.txt', transform=transform)
+val_data=MyDataset(txt='/home/molan/Desktop/project/val_new.txt', transform=transform)
 test_data=MyDataset(txt='/home/molan/Desktop/project/test_new.txt', transform=transform)
 
 train_loader = torch.utils.data.DataLoader(dataset=train_data, batch_size=16, shuffle=True)
-test_loader = torch.utils.data.DataLoader(dataset=test_data, batch_size=16)
-
+val_loader = torch.utils.data.DataLoader(dataset=val_data, batch_size=16)
+test_loader = torch.utils.data.DataLoader(dataset=test_data, batch_size=1)
 
 # In[35]:
 
@@ -372,7 +283,7 @@ optimizer = optim.Adam(net.parameters(),lr = 0.003 )
 optimizer_class = torch.optim.SGD(classification.parameters(), lr=0.02)
 loss_class = torch.nn.CrossEntropyLoss()  # the target label is NOT an one-hotted
 
-loss_history = [] 
+loss_history = []
 acc = []
 iteration_number= 0
 
@@ -402,7 +313,7 @@ for epoch in range(10):
             loss = loss_class(out, label)     # must be (1. nn output, 2. target), the target label is NOT one-hotted
             optimizer_class.zero_grad()   # clear gradients for next train
             loss.backward()         # backpropagation, compute gradients
-            optimizer_class.step()        # apply gradients 
+            optimizer_class.step()        # apply gradients
             loss_history.append(loss.cpu().item())
 
         net.eval()
@@ -410,7 +321,7 @@ for epoch in range(10):
         count = 0
         # Turn off gradients for validation, saves memory and computations
         with torch.no_grad():
-            for i, data in enumerate(test_loader,0):
+            for i, data in enumerate(val_loader,0):
                img, img_o , img_p, label, an_name = data
                img = img.cuda()
                feature = net(img)
@@ -431,9 +342,34 @@ plt.plot(acc)
 plt.title("testing accuracy")
 plt.savefig("/home/molan/Desktop/project/accuracy.png")
 
+# evaluation: clalulate precision, recall, accuracy, and F1-score.
+feature_model = torch.load('/home/molan/Desktop/project/feature_extraction_model_triple.h5')
+classidication_model = torch.load('/home/molan/Desktop/project/classification_model_triple.h5')
+feature_model.eval()
+classidication_model.eval()
+with torch.no_grad():
+    for data in test_loader:
+       img, img_o , img_p, label, an_name = data
+       img = img.cuda()
+       feature = net(img)
+       out = classification(feature)
+       prediction = torch.max(F.softmax(out), 1)[1].cpu()
+       predict.append(prediction[0])
+       target.append(label_name[an_name[0]])
+target = np.asarray(target)
+target = torch.Tensor(target).type(torch.LongTensor)
+predict = np.asarray(predict)
+predict = torch.Tensor(predict).type(torch.LongTensor)
+confusion_matrix =torch.zeros(5749,5749)
+for t, p in zip(predict.view(-1), target.view(-1)):
+    confusion_matrix[t.long(), p.long()] += 1
+precision =(confusion_matrix.diag() / confusion_matrix.sum(1))
+recall =(confusion_matrix.diag() / confusion_matrix.sum(0))
+accuracy = confusion_matrix.diag().sum() / confusion_matrix.sum()
+f1 = 2*recall*precision/(recall+precision)
 
+print('recall:',torch.mean(recall))
+print('precision: ',torch.mean(precision))
+print('F1-score: ',torch.mean(f1))
+print('accuracy: ',accuracy)
 # In[ ]:
-
-
-
-
